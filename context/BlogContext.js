6ae60126a1135fe891d1c049937cc5f7c6bb8c13
Your Name <you@example.com>
2023-11-29 23:38:49 +0300

    delete-post screen

--- context/BlogContext.js
+++ context/BlogContext.js
@@ -1,10 +1,13 @@
 import { React, useState, useReducer } from "react";
 import createDataContext from "./createDataContext";
 
+
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogPost":
-      return [...state, { title: "Angular JS", id: 3 }];
+      return [...state, { title: "Angular JS", id:Math.floor( Math.random()*9999) }];
+    case "delete_blogPost":
+      return state.filter((blogPost) => blogPost.id !== action.payload )
     default:
       return state;
   }
@@ -15,9 +18,14 @@ const addBlogPost = (dispatch) => {
     dispatch({ type: "add_blogPost" });
   };
 };
+const deleteBlogPost = (dispatch) =>{
+    return (id) => {
+        dispatch({ type: "delete_blogPost", payload: id });
+      };
+}
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,9 +1,17 @@
-import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useContext } from "react";
-import {Context} from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function HomeScreen() {
-  const { state, addBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
   return (
     <View>
       <Button title="Ekle" onPress={addBlogPost} />
@@ -11,11 +19,30 @@ export default function HomeScreen() {
         keyExtractor={(blogPosts) => blogPosts.id}
         data={state}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <MaterialIcons name="delete-forever" size={24} color="black" />
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    borderColor: "gray",
+  },
+  title: {
+    fontSize: 18,
+  },
+});
