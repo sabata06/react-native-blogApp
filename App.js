0e68bbae75ebeefc0549a3e7cd2282d74294185b
Your Name <you@example.com>
2023-11-30 16:47:50 +0300

    navigation-edit-screen created

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
 import CreateBlogScreen from "./screens/CreateBlogScreen";
 import { Provider } from "./context/BlogContext";
 import PostDetailScreen from "./screens/PostDetailScreen";
+import { AntDesign } from "@expo/vector-icons";
+import EditBlogScreen from "./screens/EditBlogScreen";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -14,9 +17,26 @@ export default function App() {
     <Provider>
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerTitle: "Blog App" }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Create")} >
+                  <AntDesign name="plus" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })}
+          />
           <Stack.Screen name="Create" component={CreateBlogScreen} />
-          <Stack.Screen name="PostDetail" component={PostDetailScreen} />
+          <Stack.Screen name="PostDetail" component={PostDetailScreen} options={({ navigation }) => ({
+              headerRight: () => (
+                <TouchableOpacity onPress={() => navigation.navigate("Edit")} >
+                  <AntDesign name="edit" size={24} color="black" />
+                </TouchableOpacity>
+              ),
+            })} />
+          <Stack.Screen name="Edit" component={EditBlogScreen}  />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
